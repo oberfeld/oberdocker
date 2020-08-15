441cc2de87085ab65705376821cff554566e2099
Emanuel Indermühle <emanuel@inthemill.ch>
2020-08-15 16:44:10 +0200

    Modularisierung des docker-compose files

---
+++ oberdocker-prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COMPOSE_PROJECT_NAME=oberdocker docker-compose \
+    -f docker-compose.yml \
+    -f docker-compose-backup.yml \
+    -f docker-compose-letsencrypt.yml \
+    -f docker-compose-es.yml \
+    "$@"
